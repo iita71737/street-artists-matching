c001707ffd9f6aa3e11ed474086cfa9155e0aefe
DanLin <iita71737@gmail.com>
2022-11-23 22:11:56 +0800

    init project

--- components/banner.js
+++ components/banner.js
@@ -4,10 +4,10 @@ const Banner = (props) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <span className={styles.title1}>Coffee</span>
-        <span className={styles.title2}>Connoisseur</span>
+        <span className={styles.title1}>Street Artist </span>
+        <span className={styles.title2}>Matching</span>
       </h1>
-      <p className={styles.subTitle}>Discover your local coffee stores!</p>
+      <p className={styles.subTitle}>Discover your local Street Artist!</p>
       <div className={styles.buttonWrapper}>
         <button className={styles.button} onClick={props.handleOnClick}>
           {props.buttonText}
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,9 @@ import { fetchCoffeeStores } from "../lib/coffee-stores";
 import useTrackLocation from "../hooks/use-track-location";
 import { ACTION_TYPES, StoreContext } from "../store/store-context";
 
+import artistsJSON from '../data/artist.json'
+import Airtable from 'airtable';
+
 export async function getStaticProps(context) {
   if (
     !process.env.NEXT_PUBLIC_FOURSQUARE_API_KEY &&
@@ -25,11 +28,42 @@ export async function getStaticProps(context) {
     };
   }
   const coffeeStores = await fetchCoffeeStores();
+ 
+  const airtable = new Airtable({
+    apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY ,
+  });
+
+  const records = await airtable
+    .base(process.env.NEXT_PUBLIC_AIRTABLE_BASE_KEY)('street-artist-list')
+    .select({
+      // maxRecords: 3,
+      // fields: ['certificateNo', 'name', 'id', 'category', 'subcategory'],
+    })
+    .all();
+
+  const artists = records.map((record) => {
+    return {
+      id: record.get('id'),
+      certificateNo : record.get('certificateNo'),
+      name: record.get('name'),
+      category: record.get('category') ? record.get('category') : null,
+      subcategory: record.get('subcategory') ? record.get('subcategory') : null,
+      subcategoryDes: record.get('subcategoryDes') ?  record.get('subcategoryDes') : null,
+      introUrl: record.get('introUrl') ? record.get('introUrl') : null,
+      stageName: record.get('stageName') ? record.get('stageName') : null,
+      set: record.get('sex') ? record.get('sex') : null,
+      phone: record.get('phone') ? record.get('phone') : null,
+      email: record.get('email') ? record.get('email') : null
+    };
+  });
+
+  console.log(artists,'-artists')
 
   return {
     props: {
       coffeeStores,
-    }, // will be passed to the page component as props
+      artists : artists || null
+    }
   };
 }
 
@@ -53,7 +87,6 @@ export default function Home(props) {
 
           const coffeeStores = await response.json();
 
-          // setCoffeeStores(fetchedCoffeeStores);
           dispatch({
             type: ACTION_TYPES.SET_COFFEE_STORES,
             payload: {
@@ -61,9 +94,9 @@ export default function Home(props) {
             },
           });
           setCoffeeStoresError("");
-          //set coffee stores
+
         } catch (error) {
-          //set error
+
           console.error({ error });
           setCoffeeStoresError(error.message);
         }
@@ -75,10 +108,12 @@ export default function Home(props) {
   const handleOnBannerBtnClick = () => {
     handleTrackLocation();
   };
+
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Coffee Connoisseur</title>
+        <title>Street Artist</title>
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
@@ -88,23 +123,23 @@ export default function Home(props) {
 
       <main className={styles.main}>
         <Banner
-          buttonText={isFindingLocation ? "Locating..." : "View stores nearby"}
+          buttonText={isFindingLocation ? "Locating..." : "View artist nearby"}
           handleOnClick={handleOnBannerBtnClick}
         />
         {locationErrorMsg && <p>Something went wrong: {locationErrorMsg}</p>}
         {coffeeStoresError && <p>Something went wrong: {coffeeStoresError}</p>}
         <div className={styles.heroImage}>
           <Image
-            src="/static/hero-image.png"
-            width={700}
-            height={400}
+            src="/static/logo-mini.png"
+            width={300}
+            height={300}
             alt="hero image"
           />
         </div>
 
         {coffeeStores.length > 0 && (
           <div className={styles.sectionWrapper}>
-            <h2 className={styles.heading2}>Stores near me</h2>
+            <h2 className={styles.heading2}>Show near me</h2>
             <div className={styles.cardLayout}>
               {coffeeStores.map((coffeeStore) => {
                 return (
@@ -123,6 +158,29 @@ export default function Home(props) {
             </div>
           </div>
         )}
+
+      {props.artists && props.artists.length > 0 && (
+        <div className={styles.sectionWrapper}>
+        <h2 className={styles.heading2}>Show all artists</h2>
+        <div className={styles.cardLayout}>
+        { props.artists.map(artist => {
+          return (
+            <Card
+            key={artist.id}
+            name={artist.name}
+            imgUrl={
+              artist.imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
+            href={`/coffee-store/${artist.id}`}
+            className={styles.card}
+            />
+            )
+        }) 
+        }
+          </div>
+      </div>
+      )}
 
         <div className={styles.sectionWrapper}>
           {props.coffeeStores.length > 0 && (
